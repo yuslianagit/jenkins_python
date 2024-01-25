0043fa4fbc0e80e9d0c4749af2ce0ea70c9f4f61
yuslianagit <133191607+yuslianagit@users.noreply.github.com>
2024-01-25 18:07:39 +0800

    Update testcal.py

--- testcal.py
+++ testcal.py
@@ -1,10 +1,26 @@
 import unittest
-
-class testcal(unittest.TestCase):
-  def test_msg(self):
-    a = 'some'
-    b = 'some'
-    self.assertEqual(a,b)
-
+from unittest.mock import patch
+from menu_selection import SystemMonitor
+ 
+class Unittest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # This method is executed once before all the test methods in this class
+        cls.monitor = SystemMonitor()
+ 
+    @patch('psutil.disk_usage')
+    def test_check_disk_usage_normal(self, mock_disk_usage):
+        # Test the normal case where disk usage is below the threshold
+        mock_disk_usage.return_value.percent = 25
+        self.assertTrue(self.monitor.check_disk_usage())
+ 
+    @patch('psutil.disk_usage')
+    def test_check_disk_usage_high(self, mock_disk_usage):
+        # Test the case where disk usage is above the threshold
+        mock_disk_usage.return_value.percent = 80
+        self.assertFalse(self.monitor.check_disk_usage())
+ 
+    # Add more test cases as needed
+ 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
